f7e07b42cd70da070cb2c78ff14a8eefaaa7fd93
Rami-Alfarai <ramifarseabdofaraalkhtib@gmail.com>
2025-09-30 12:01:41 +0300

    adding  layout.createParallelGroup in the project

--- src/main.java
+++ src/main.java
@@ -210,8 +210,9 @@ public class main extends javax.swing.JFrame {
         layout.setVerticalGroup(
 
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            
+
             .addGroup(layout.createSequentialGroup()
+            
                 .addContainerGap()
                 .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
