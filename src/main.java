c25a849e0b382e5e8b19a4854bce2722c02a385d
Rami-Alfarai <ramifarseabdofaraalkhtib@gmail.com>
2025-09-30 11:09:47 +0300

    adding jpanel1

--- src/main.java
+++ src/main.java
@@ -13,7 +13,7 @@ public class main extends javax.swing.JFrame {
     public main() {
         
         initComponents();
-        
+
         
     }
 
@@ -23,6 +23,7 @@ public class main extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+
         jLabel1 = new javax.swing.JLabel();
         stack = new javax.swing.JButton();
         list = new javax.swing.JButton();
