5bbcd62ec8bc0723807afc679966ce18727882fc
Rami-Alfarai <ramifarseabdofaraalkhtib@gmail.com>
2025-09-30 12:20:46 +0300

    adding     private javax.swing.JButton linked_list in the project

--- src/main.java
+++ src/main.java
@@ -335,8 +335,9 @@ public class main extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
 
     private javax.swing.JPanel jPanel1;
-    
+
     private javax.swing.JButton linked_list;
+    
     private javax.swing.JButton list;
     private javax.swing.JButton queue;
     private javax.swing.JButton stack;
