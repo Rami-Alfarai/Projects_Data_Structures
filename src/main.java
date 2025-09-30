41681c1805f1afe8c193cc496738c079e927542f
Rami-Alfarai <ramifarseabdofaraalkhtib@gmail.com>
2025-09-30 12:15:09 +0300

    adding  catch  ClassNotFoundException  in the project

--- src/main.java
+++ src/main.java
@@ -293,14 +293,15 @@ public class main extends javax.swing.JFrame {
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
         try {
-            
+
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
+        }
+         catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
             java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
