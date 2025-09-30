74d7b8707e33f629ffaea53b338890e2c66f2491
Rami-Alfarai <ramifarseabdofaraalkhtib@gmail.com>
2025-09-30 12:02:14 +0300

    adding addGroup layout.createSequentialGroup in the project

--- src/main.java
+++ src/main.java
@@ -212,8 +212,9 @@ public class main extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
 
             .addGroup(layout.createSequentialGroup()
-            
+
                 .addContainerGap()
+                
                 .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
