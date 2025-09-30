9caedfb22e60f52ce1baaea2cb609fdd8df9ad10
Rami-Alfarai <ramifarseabdofaraalkhtib@gmail.com>
2025-09-30 12:17:14 +0300

    adding  catch (javax.swing.UnsupportedLookAndFeelException ex)  in the project

--- src/main.java
+++ src/main.java
@@ -310,8 +310,10 @@ public class main extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } 
         catch (IllegalAccessException ex) {
+
             java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } 
+        catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
