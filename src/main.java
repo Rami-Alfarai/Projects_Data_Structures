375a7ef06186d918d4b6b9b0bf0b94e2d3e0bc5f
Rami-Alfarai <ramifarseabdofaraalkhtib@gmail.com>
2025-09-30 12:16:37 +0300

    adding  catch (IllegalAccessException  in the project

--- src/main.java
+++ src/main.java
@@ -304,10 +304,12 @@ public class main extends javax.swing.JFrame {
          catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        
+
          catch (InstantiationException ex) {
+
             java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
+        } 
+        catch (IllegalAccessException ex) {
             java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
