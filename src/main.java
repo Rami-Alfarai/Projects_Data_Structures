8d15c4656ec383d489720fd9cdc6e92ee37e8b19
Rami-Alfarai <ramifarseabdofaraalkhtib@gmail.com>
2025-09-30 11:57:55 +0300

    adding addGroup javax.swing.GroupLayout in the project

--- src/main.java
+++ src/main.java
@@ -198,8 +198,9 @@ public class main extends javax.swing.JFrame {
         layout.setHorizontalGroup(
 
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            
+
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
